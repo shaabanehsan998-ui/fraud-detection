33016eb4358aa6c8da71493b8ff2aacdf0cb5de6
shaabanehsan998-ui <shaabanehsan998@gmail.com>
2026-05-21 13:18:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,15 @@
 from fastapi import FastAPI
-import joblib
-import numpy as np
 
 app = FastAPI()
-
-model = joblib.load("model.pkl")
-scaler = joblib.load("scaler.pkl") 
 
 @app.get("/")
 def home():
     return {
-        "message": "Fraud Detection API Running"
+        "message": "NEW VERSION WORKING"
     }
 
-
 @app.post("/predict")
-def predict(data: dict):
-
-    try:
-
-        features = np.array(data["features"], dtype=float)
-
-        features = features.reshape(1, -1)
-
-        features_scaled = scaler.transform(features)
-
-        prediction = model.predict(features_scaled)
-
-        return {
-            "prediction_raw": str(prediction),
-            "prediction_shape": str(prediction.shape),
-            "prediction_type": str(type(prediction))
-        }
-
-    except Exception as e:
-
-        return {
-            "error": str(e)
-        }
+def predict():
+    return {
+        "status": "API UPDATED SUCCESSFULLY"
+    }
