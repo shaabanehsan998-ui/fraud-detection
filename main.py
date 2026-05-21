b8ca4973e69bb4b79552d9407207bab222612e9c
shaabanehsan998-ui <shaabanehsan998@gmail.com>
2026-05-21 13:50:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,45 +8,20 @@ app = FastAPI()
 model = joblib.load("model.pkl")
 scaler = joblib.load("scaler.pkl")
 
-
-@app.get("/")
-def home():
-    return {
-        "message": "Fraud Detection API Running"
-    }
-
-
 @app.post("/predict")
 def predict(data: dict):
 
+    features = np.array(data["features"], dtype=float).reshape(1, -1)
+
     try:
-
-        # أخذ البيانات
-        features = data["features"]
-
-        # تحويلها لمصفوفة
-        features = np.array(features, dtype=float)
-
-        # تحويلها لشكل مناسب
-        features = features.reshape(1, -1)
-
-        # scaling
-        features_scaled = scaler.transform(features)
-
-        # prediction
-        prediction = model.predict(features_scaled)
-
-        # probability
-        probability = model.predict_proba(features_scaled)
+        scaled = scaler.transform(features)
+        pred = model.predict(scaled)
 
         return {
-            "prediction": int(prediction[0]),
-            "fraud_probability": float(probability[0][1])
+            "prediction": int(pred[0])
         }
 
     except Exception as e:
-
         return {
-            "prediction": int(prediction[0]),
-            "probability": str(probability)
+            "error": str(e)
         }
