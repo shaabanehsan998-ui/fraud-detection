a4310724740e4727248f434eb0f7af136da92f81
shaabanehsan998-ui <shaabanehsan998@gmail.com>
2026-05-21 13:28:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,51 @@
 from fastapi import FastAPI
+import joblib
+import numpy as np
 
 app = FastAPI()
+
+# تحميل النموذج والـ scaler
+model = joblib.load("model.pkl")
+scaler = joblib.load("scaler.pkl")
 
 
 @app.get("/")
 def home():
     return {
-        "message": "NEW VERSION WORKING"
+        "message": "Fraud Detection API Running"
     }
 
+
 @app.post("/predict")
-def predict():
-    return {
-        "status": "API UPDATED SUCCESSFULLY"
-    }
+def predict(data: dict):
+
+    try:
+
+        # أخذ البيانات
+        features = data["features"]
+
+        # تحويلها لمصفوفة
+        features = np.array(features, dtype=float)
+
+        # تحويلها لشكل مناسب
+        features = features.reshape(1, -1)
+
+        # scaling
+        features_scaled = scaler.transform(features)
+
+        # prediction
+        prediction = model.predict(features_scaled)
+
+        # probability
+        probability = model.predict_proba(features_scaled)
+
+        return {
+            "prediction": int(prediction[0]),
+            "fraud_probability": float(probability[0][1])
+        }
+
+    except Exception as e:
+
+        return {
+            "error": str(e)
+        }
